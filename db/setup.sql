10f129a905986c38f64e729f4c5ae15958176752
David Chima <chima2g@hotmail.com>
2019-05-13 11:39:00 +0100

    initial commit

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS be_nc_news_generated;
+CREATE DATABASE be_nc_news_generated;
+
+DROP DATABASE IF EXISTS be_nc_news_generated_test;
+CREATE DATABASE be_nc_news_generated_test;
